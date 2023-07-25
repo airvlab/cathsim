cb3a4bbef41e23db04cfaab6a43c596e897a1ce0
Tudor Jianu <tudorjnu@gmail.com>
2023-07-25 13:59:21 +0100

    refactor

--- cathsim/cathsim/env_utils.py
+++ cathsim/cathsim/env_utils.py
@@ -19,15 +19,15 @@ def distance(a: np.ndarray, b: np.ndarray) -> np.ndarray:
 
 
 def make_dm_env(
-        phantom: str = 'phantom3',
-        target: str = 'bca',
-        use_pixels: bool = False,
-        dense_reward: bool = True,
-        success_reward: float = 10.0,
-        delta: float = 0.004,
-        use_segment: bool = False,
-        image_size: int = 80,
-        **kwargs,
+    phantom: str = "phantom3",
+    target: str = "bca",
+    use_pixels: bool = False,
+    dense_reward: bool = True,
+    success_reward: float = 10.0,
+    delta: float = 0.004,
+    use_segment: bool = False,
+    image_size: int = 80,
+    **kwargs,
 ) -> composer.Environment:
     """
     Makes a dm_control environment.
@@ -69,42 +69,49 @@ def make_dm_env(
     return env
 
 
-def make_gym_env(config: dict = dict(task_kwargs={}, wrapper_kwargs={}, render_kwargs={},)) -> gym.Env:
-    wrapper_kwargs = config['wrapper_kwargs']
-    env_kwargs = config['env_kwargs']
-    task_kwargs = config['task_kwargs']
+def make_gym_env(
+    config: dict = dict(
+        task_kwargs={},
+        wrapper_kwargs={},
+        render_kwargs={},
+    )
+) -> gym.Env:
+    wrapper_kwargs = config["wrapper_kwargs"]
+    env_kwargs = config["env_kwargs"]
+    task_kwargs = config["task_kwargs"]
 
-    gym_version = env_kwargs['gym_version']
-    if gym_version == 'gym':
+    gym_version = env_kwargs["gym_version"]
+    if gym_version == "gym":
         from cathsim.wrappers import DMEnvToGymWrapper
         from gym import wrappers
-    elif gym_version == 'gymnasium':
+    elif gym_version == "gymnasium":
         from wrapper_gymnasium import DMEnvToGymWrapper
         from gymnasium import wrappers
 
-    max_episode_steps = wrapper_kwargs.get('time_limit', 300)
-    filter_keys = wrapper_kwargs.get('use_obs', None)
-    flatten_observation = wrapper_kwargs.get('flatten_obs', False)
-    grayscale = wrapper_kwargs.get('grayscale', False)
-    normalize_obs = wrapper_kwargs.get('normalize_obs', False)
-    frame_stack = wrapper_kwargs.get('frame_stack', 1)
-    use_pixels = task_kwargs.get('use_pixels', False)
-    goal_env = wrapper_kwargs.get('goal_env', False)
+    max_episode_steps = wrapper_kwargs.get("time_limit", 300)
+    filter_keys = wrapper_kwargs.get("use_obs", None)
+    flatten_observation = wrapper_kwargs.get("flatten_obs", False)
+    grayscale = wrapper_kwargs.get("grayscale", False)
+    normalize_obs = wrapper_kwargs.get("normalize_obs", False)
+    frame_stack = wrapper_kwargs.get("frame_stack", 1)
+    use_pixels = task_kwargs.get("use_pixels", False)
+    goal_env = wrapper_kwargs.get("goal_env", False)
 
     if goal_env:
-        filter_keys = filter_keys + ['achieved_goal', 'desired_goal']
+        filter_keys = filter_keys + ["achieved_goal", "desired_goal"]
 
     env = make_dm_env(**task_kwargs)
     env = DMEnvToGymWrapper(env=env, env_kwargs=env_kwargs)
 
     if goal_env:
         from cathsim.wrappers import GoalEnvWrapper
+
         env = GoalEnvWrapper(env=env)
 
     env = wrappers.TimeLimit(env, max_episode_steps=max_episode_steps)
 
-    if gym_version == 'gymnasium':
-        env = wrappers.EnvCompatibility(env, render_mode='rgb_array')
+    if gym_version == "gymnasium":
+        env = wrappers.EnvCompatibility(env, render_mode="rgb_array")
 
     if filter_keys:
         env = wrappers.FilterObservation(env, filter_keys=filter_keys)
@@ -114,17 +121,21 @@ def make_gym_env(config: dict = dict(task_kwargs={}, wrapper_kwargs={}, render_k
 
     if use_pixels:
         from cathsim.wrappers import MultiInputImageWrapper
+
         env = MultiInputImageWrapper(
             env,
             grayscale=grayscale,
-            image_key=wrapper_kwargs.get('image_key', 'pixels'),
-            keep_dim=wrapper_kwargs.get('keep_dim', True),
-            channel_first=wrapper_kwargs.get('channel_first', False),
+            image_key=wrapper_kwargs.get("image_key", "pixels"),
+            keep_dim=wrapper_kwargs.get("keep_dim", True),
+            channel_first=wrapper_kwargs.get("channel_first", False),
         )
 
-    if wrapper_kwargs.get('dict2array', False):
-        assert (len(env.observation_space.spaces) == 1), 'Only one observation is allowed.'
+    if wrapper_kwargs.get("dict2array", False):
+        assert (
+            len(env.observation_space.spaces) == 1
+        ), "Only one observation is allowed."
         from cathsim.wrappers import Dict2Array
+
         env = Dict2Array(env)
 
     if normalize_obs:
@@ -137,7 +148,6 @@ def make_gym_env(config: dict = dict(task_kwargs={}, wrapper_kwargs={}, render_k
 
 
 class Application(Application):
-
     def __init__(self, title, width, height):
         super().__init__(title, width, height)
         from dm_control.viewer import user_input
@@ -158,7 +168,7 @@ class Application(Application):
         time_step = self._runtime._time_step
         physics = self._runtime._env.physics
         force = self._runtime._env.task.get_force(physics).round(2)
-        print(f'step {self._step:03} | force {force}')
+        print(f"step {self._step:03} | force {force}")
         if not time_step.last():
             self._advance_simulation()
             self._step += 1
@@ -182,16 +192,23 @@ class Application(Application):
         self.perform_action()
 
 
-def launch(environment_loader, policy=None, title='Explorer', width=1024,
-           height=768, trial_path=None):
+def launch(
+    environment_loader,
+    policy=None,
+    title="Explorer",
+    width=1024,
+    height=768,
+    trial_path=None,
+):
     app = Application(title=title, width=width, height=height)
     app.launch(environment_loader=environment_loader, policy=policy)
 
 
 if __name__ == "__main__":
     from rl.utils import get_config
-    config = get_config('test')
-    __import__('pprint').pprint(config)
+
+    config = get_config("test")
+    __import__("pprint").pprint(config)
     # env = make_env(config)
     env = make_dm_env()
     launch(env, policy=None)
--- rl/utils.py
+++ rl/utils.py
@@ -31,14 +31,14 @@ class CnnPolicy(policies.ActorCriticCnnPolicy):
 
 
 ALGOS = {
-    'ppo': PPO,
-    'sac': SAC,
-    'bc': bc,
+    "ppo": PPO,
+    "sac": SAC,
+    "bc": bc,
 }
 
-RESULTS_PATH = Path(__file__).parent.parent / 'results'
-EXPERIMENT_PATH = RESULTS_PATH / 'experiments'
-EVALUATION_PATH = RESULTS_PATH / 'evaluation'
+RESULTS_PATH = Path(__file__).parent.parent / "results"
+EXPERIMENT_PATH = RESULTS_PATH / "experiments"
+EVALUATION_PATH = RESULTS_PATH / "evaluation"
 
 
 def make_experiment(experiment_path: Path = None) -> tuple:
@@ -54,23 +54,17 @@ def make_experiment(experiment_path: Path = None) -> tuple:
     example:
         model_path, log_path, eval_path = make_experiment('test')
     """
-    assert experiment_path, 'experiment_path must be specified'
+    assert experiment_path, "experiment_path must be specified"
     experiment_path = EXPERIMENT_PATH / experiment_path
-    model_path = experiment_path / 'models'
-    eval_path = experiment_path / 'eval'
-    log_path = experiment_path / 'logs'
+    model_path = experiment_path / "models"
+    eval_path = experiment_path / "eval"
+    log_path = experiment_path / "logs"
     for dir in [experiment_path, model_path, log_path, eval_path]:
         dir.mkdir(parents=True, exist_ok=True)
     return model_path, log_path, eval_path
 
 
-def make_env(
-    n_envs: int = 1,
-    config: dict = {},
-    monitor_wrapper=True,
-    **kwargs
-):
-
+def make_env(n_envs: int = 1, config: dict = {}, monitor_wrapper=True, **kwargs):
     def _init() -> gym.Env:
         env = make_gym_env(config=config)
         return env
@@ -79,42 +73,45 @@ def make_env(
         env = DummyVecEnv([_init])
     else:
         env = SubprocVecEnv([_init for _ in range(n_envs)])
-
     if monitor_wrapper:
         if n_envs == 1:
             from stable_baselines3.common.monitor import Monitor
+
             env = Monitor(env)
         else:
             from stable_baselines3.common.vec_env import VecMonitor
+
             env = VecMonitor(env)
 
     return env
 
 
-def train(algo: str,
-          experiment: str,
-          experiment_path: Path = None,
-          n_runs: int = 4,
-          time_steps: int = 500_000,
-          evaluate: bool = False,
-          device: str = None,
-          n_envs: int = None,
-          vec_env: bool = True,
-          config: dict = {},
-          **kwargs):
+def train(
+    algo: str,
+    experiment: str,
+    experiment_path: Path = None,
+    n_runs: int = 4,
+    time_steps: int = 500_000,
+    evaluate: bool = False,
+    device: str = None,
+    n_envs: int = None,
+    vec_env: bool = True,
+    config: dict = {},
+    **kwargs,
+):
     from rl.evaluation import evaluate_policy
-    algo_kwargs = config.get('algo_kwargs', {})
+
+    algo_kwargs = config.get("algo_kwargs", {})
 
     if not device:
-        device = 'cuda' if th.cuda.is_available() else 'cpu'
+        device = "cuda" if th.cuda.is_available() else "cpu"
     n_envs = n_envs or os.cpu_count() // 2
 
-    assert algo in ALGOS.keys(), f'algo must be one of {ALGOS.keys()}'
-    assert experiment_path, 'experiment_path must be specified'
-    assert n_runs > 0, 'n_runs must be greater than 0'
-    assert time_steps > 0, 'time_steps must be greater than 0'
-    assert device in ['cpu', 'cuda'], 'device must be one of [cpu, cuda]'
-    assert n_envs > 0, 'n_envs must be greater than 0'
+    assert algo in ALGOS.keys(), f"algo must be one of {ALGOS.keys()}"
+    assert n_runs > 0, "n_runs must be greater than 0"
+    assert time_steps > 0, "time_steps must be greater than 0"
+    assert device in ["cpu", "cuda"], "device must be one of [cpu, cuda]"
+    assert n_envs > 0, "n_envs must be greater than 0"
 
     experiment_path = experiment_path / experiment
     model_path, log_path, eval_path = make_experiment(experiment_path)
@@ -124,48 +121,53 @@ def train(algo: str,
     env = make_env(n_envs=n_envs, config=config)
 
     for seed in range(n_runs):
-        if (model_path / f'{algo}_{seed}.zip').exists():
-            print(f'Model {algo} {seed} already exists, skipping')
+        if (model_path / f"{algo}_{seed}.zip").exists():
+            print(f"Model {algo} {seed} already exists, skipping")
             pass
         else:
             for key, value in algo_kwargs.items():
-                __import__('pprint').pprint(f'{key}: {value}')
-            model = ALGOS[algo](algo_kwargs.get('policy', 'MlpPolicy'),
-                                env,
-                                device=device,
-                                verbose=1,
-                                tensorboard_log=log_path,
-                                policy_kwargs=algo_kwargs.get('policy_kwargs', {}),
-                                **kwargs)
+                __import__("pprint").pprint(f"{key}: {value}")
+            model = ALGOS[algo](
+                algo_kwargs.get("policy", "MlpPolicy"),
+                env,
+                device=device,
+                verbose=1,
+                tensorboard_log=log_path,
+                policy_kwargs=algo_kwargs.get("policy_kwargs", {}),
+                **kwargs,
+            )
 
-            model.learn(total_timesteps=time_steps,
-                        log_interval=10,
-                        tb_log_name=f'{algo}_{seed}',
-                        progress_bar=True,
-                        reset_num_timesteps=False)
+            model.learn(
+                total_timesteps=time_steps,
+                log_interval=10,
+                tb_log_name=f"{algo}_{seed}",
+                progress_bar=True,
+                reset_num_timesteps=False,
+            )
 
-            model.save(model_path / f'{algo}_{seed}.zip')
+            model.save(model_path / f"{algo}_{seed}.zip")
 
             if evaluate:
                 results = evaluate_policy(model, env, 10)
-                np.savez_compressed(eval_path / f'{algo}_{seed}', **results)
+                np.savez_compressed(eval_path / f"{algo}_{seed}", **results)
             th.cuda.empty_cache()
 
 
 def cmd_visualize_agent(args=None):
     import cv2
-    from cathsim.cathsim import make_env
+    from cathsim.cathsim.env_utils import make_gym_env
     import argparse as ap
-    from scratch.bc.custom_networks import CustomPolicy
+
+    # from scratch.bc.custom_networks import CustomPolicy
     parser = ap.ArgumentParser()
-    parser.add_argument('--config', type=str, default='full')
-    parser.add_argument('--phantom', type=str, default='phantom3')
-    parser.add_argument('--target', type=str, default='bca')
-    parser.add_argument('--seed', type=int, default=None)
-    parser.add_argument('--save-video', type=bool, default=False)
-    parser.add_argument('--get_images', type=bool, default=False)
-    parser.add_argument('--algo', type=str, default='sac')
-    parser.add_argument('--visualize-sites', type=bool, default=False)
+    parser.add_argument("--config", type=str, default="full")
+    parser.add_argument("--phantom", type=str, default="phantom3")
+    parser.add_argument("--target", type=str, default="bca")
+    parser.add_argument("--seed", type=int, default=None)
+    parser.add_argument("--save-video", type=bool, default=False)
+    parser.add_argument("--get_images", type=bool, default=False)
+    parser.add_argument("--algo", type=str, default="sac")
+    parser.add_argument("--visualize-sites", type=bool, default=False)
     args = parser.parse_args()
 
     algo = args.algo
@@ -173,35 +175,36 @@ def cmd_visualize_agent(args=None):
     if args.save_video:
         import moviepy.editor as mpy
 
-    path = Path(f'{args.phantom}/{args.target}/{args.config}')
+    path = Path(f"{args.phantom}/{args.target}/{args.config}")
     config = get_config(args.config)
     model_path, log_path, eval_path = make_experiment(path)
 
     if args.seed is None:
         model_path = model_path / algo
     else:
-        model_path = model_path / (algo + f'_{args.seed}.zip')
-    video_path = model_path.parent.parent / 'videos'
-    images_path = model_path.parent.parent / 'images'
+        model_path = model_path / (algo + f"_{args.seed}.zip")
+    video_path = model_path.parent.parent / "videos"
+    images_path = model_path.parent.parent / "images"
 
-    assert model_path.exists(), f'{model_path} does not exist'
+    assert model_path.exists(), f"{model_path} does not exist"
 
     # if config == 'pixels':
     #     config['wrapper_kwargs']['get_images'] = args.get_images
-    config['task_kwargs']['target'] = args.target
-    config['task_kwargs']['phantom'] = args.phantom
-    config['task_kwargs']['visualize_sites'] = args.visualize_sites
+    config["task_kwargs"]["target"] = args.target
+    config["task_kwargs"]["phantom"] = args.phantom
+    config["task_kwargs"]["visualize_sites"] = args.visualize_sites
 
-    if algo == 'bc':
-        config['wrapper_kwargs']['channel_first'] = True
-        env = make_env(config=config)
+    if algo == "bc":
+        config["wrapper_kwargs"]["channel_first"] = True
+        env = make_gym_env(config=config)
         env = Dict2Array(env)
-        model = CnnPolicy(observation_space=env.observation_space,
-                          action_space=env.action_space,
-                          lr_schedule=lambda _: th.finfo(th.float32).max,
-                          ).load(model_path)
+        model = CnnPolicy(
+            observation_space=env.observation_space,
+            action_space=env.action_space,
+            lr_schedule=lambda _: th.finfo(th.float32).max,
+        ).load(model_path)
     else:
-        env = make_env(config=config)
+        env = make_gym_env(config=config)
         model = ALGOS[algo].load(model_path)
 
     for episode in range(1):
@@ -217,7 +220,7 @@ def cmd_visualize_agent(args=None):
 
             rewards.append(reward)
 
-            image = env.render('rgb_array', image_size=480)
+            image = env.render("rgb_array", image_size=480)
             # segment_image = env.env.env.env.physics.render(480, 480, camera_id=0, scene_option=scene_option,
             #                                                segmentation=True)
             # segment_image = filter_mask(segment_image)
@@ -227,26 +230,31 @@ def cmd_visualize_agent(args=None):
 
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
             # cv2.imshow('segment', segment_image)
-            cv2.imshow('image', image)
+            cv2.imshow("image", image)
             cv2.waitKey(1)
 
         if args.save_video:
             os.makedirs(video_path, exist_ok=True)
-            frames = [mpy.ImageClip(m).set_duration(0.1)for m in frames]
-            clip = mpy.concatenate_videoclips(frames, method='compose')
+            frames = [mpy.ImageClip(m).set_duration(0.1) for m in frames]
+            clip = mpy.concatenate_videoclips(frames, method="compose")
             clip.write_videofile(
-                (video_path / f'{algo}_{episode}.mp4').as_posix(), fps=60)
+                (video_path / f"{algo}_{episode}.mp4").as_posix(), fps=60
+            )
         if args.get_images:
             import matplotlib.pyplot as plt
+
             os.makedirs(images_path, exist_ok=True)
-            print(f'Saving images to {images_path}')
+            print(f"Saving images to {images_path}")
             for i in range(len(frames)):
-                plt.imsave(f'{images_path}/{algo}_{episode}_{i}.png', frames[i])
-                plt.imsave(f'{images_path}/{algo}_{episode}_{i}_segment.png', segment_frames[i], cmap='gray')
+                plt.imsave(f"{images_path}/{algo}_{episode}_{i}.png", frames[i])
+                plt.imsave(
+                    f"{images_path}/{algo}_{episode}_{i}_segment.png",
+                    segment_frames[i],
+                    cmap="gray",
+                )
 
 
 class Application(Application):
-
     def __init__(self, title, width, height, trial_path=None):
         super().__init__(title, width, height)
         from dm_control.viewer import user_input
@@ -261,39 +269,39 @@ class Application(Application):
         if trial_path.exists():
             episode_paths = sorted(trial_path.iterdir())
             if len(episode_paths) > 0:
-                episode_num = episode_paths[-1].name.split('_')[-1]
+                episode_num = episode_paths[-1].name.split("_")[-1]
                 self._episode = int(episode_num) + 1
         self._policy = None
         self._trajectory = {}
         self._trial_path = trial_path
-        self._episode_path = self._trial_path / f'episode_{self._episode:02}'
-        self._images_path = self._episode_path / 'images'
+        self._episode_path = self._trial_path / f"episode_{self._episode:02}"
+        self._images_path = self._episode_path / "images"
         self._images_path.mkdir(parents=True, exist_ok=True)
 
     def _save_transition(self, observation, action):
         for key, value in observation.items():
-            if key != 'top_camera':
+            if key != "top_camera":
                 self._trajectory.setdefault(key, []).append(value)
             else:
-                image_path = self._images_path / f'{self._step:03}.png'
+                image_path = self._images_path / f"{self._step:03}.png"
                 plt.imsave(image_path.as_posix(), value)
-        self._trajectory.setdefault('action', []).append(action)
+        self._trajectory.setdefault("action", []).append(action)
 
     def _initialize_episode(self):
-        trajectory_path = self._episode_path / 'trajectory'
+        trajectory_path = self._episode_path / "trajectory"
         np.savez_compressed(trajectory_path.as_posix(), **self._trajectory)
         self._restart_runtime()
-        print(f'Episode {self._episode:02} finished')
+        print(f"Episode {self._episode:02} finished")
         self._trajectory = {}
         self._step = 0
         self._episode += 1
         # change the episode path to the new episode
-        self._episode_path = self._trial_path / f'episode_{self._episode:02}'
-        self._images_path = self._episode_path / 'images'
+        self._episode_path = self._trial_path / f"episode_{self._episode:02}"
+        self._images_path = self._episode_path / "images"
         self._images_path.mkdir(parents=True, exist_ok=True)
 
     def perform_action(self):
-        print(f'step {self._step:03}')
+        print(f"step {self._step:03}")
         time_step = self._runtime._time_step
         if not time_step.last():
             self._advance_simulation()
@@ -320,10 +328,15 @@ class Application(Application):
         self.perform_action()
 
 
-def launch(environment_loader, policy=None, title='Explorer', width=1024,
-           height=768, trial_path=None):
-    app = Application(title=title, width=width, height=height,
-                      trial_path=trial_path)
+def launch(
+    environment_loader,
+    policy=None,
+    title="Explorer",
+    width=1024,
+    height=768,
+    trial_path=None,
+):
+    app = Application(title=title, width=width, height=height, trial_path=trial_path)
     app.launch(environment_loader=environment_loader, policy=policy)
 
 
@@ -332,17 +345,16 @@ def record_expert_trajectories(trial_name: Path):
     from cathsim.cathsim.phantom import Phantom
     from dm_control import composer
 
-    trial_path = Path(__file__).parent.parent / 'expert' / trial_name
+    trial_path = Path(__file__).parent.parent / "expert" / trial_name
     try:
         trial_path.mkdir(parents=True)
     except FileExistsError:
-        cont_training = input(
-            f'Trial {trial_name} already exists. Continue? [y/N] ')
-        cont_training = 'n' if cont_training == '' else cont_training
-        if cont_training.lower() == 'y':
+        cont_training = input(f"Trial {trial_name} already exists. Continue? [y/N] ")
+        cont_training = "n" if cont_training == "" else cont_training
+        if cont_training.lower() == "y":
             pass
         else:
-            print('Aborting')
+            print("Aborting")
             exit()
 
     phantom = Phantom()
@@ -361,20 +373,21 @@ def record_expert_trajectories(trial_name: Path):
         strip_singleton_obs_buffer_dim=True,
     )
 
-    action_spec_name = '\t' + env.action_spec().name.replace('\t', '\n\t')
-    print('\nAction Spec:\n', action_spec_name)
+    action_spec_name = "\t" + env.action_spec().name.replace("\t", "\n\t")
+    print("\nAction Spec:\n", action_spec_name)
     time_step = env.reset()
-    print('\nObservation Spec:')
+    print("\nObservation Spec:")
     for key, value in time_step.observation.items():
-        print('\t', key, value.shape)
+        print("\t", key, value.shape)
 
     launch(env, trial_path=trial_path)
 
 
 def cmd_record_traj(args=None):
     import argparse
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--trial_name', type=str, default='test')
+    parser.add_argument("-n", "--trial_name", type=str, default="test")
     args = parser.parse_args(args)
     record_expert_trajectories(Path(args.trial_name))
 
@@ -384,37 +397,39 @@ def get_config(config_name):
     from mergedeep import merge
     from rl.custom_extractor import CustomExtractor
 
-    configs_path = Path(__file__).parent / 'config'
-    main_config_path = configs_path / 'main.yaml'
-    config_path = configs_path / (config_name + '.yaml')
-    main_config = yaml.safe_load(open(main_config_path, 'r'))
-    config = yaml.safe_load(open(config_path, 'r'))
+    configs_path = Path(__file__).parent / "config"
+    main_config_path = configs_path / "main.yaml"
+    config_path = configs_path / (config_name + ".yaml")
+    main_config = yaml.safe_load(open(main_config_path, "r"))
+    config = yaml.safe_load(open(config_path, "r"))
     config = merge(main_config, config)
 
-    policy_kwargs = main_config['algo_kwargs'].get('policy_kwargs', {})
-    feature_extractor_class = policy_kwargs.get('features_extractor_class', None)
-    if feature_extractor_class == 'CustomExtractor':
-        main_config['algo_kwargs']['policy_kwargs']['features_extractor_class'] = CustomExtractor
+    policy_kwargs = main_config["algo_kwargs"].get("policy_kwargs", {})
+    feature_extractor_class = policy_kwargs.get("features_extractor_class", None)
+    if feature_extractor_class == "CustomExtractor":
+        main_config["algo_kwargs"]["policy_kwargs"][
+            "features_extractor_class"
+        ] = CustomExtractor
 
-    if main_config['algo_kwargs']['replay_buffer_class'] == 'HerReplayBuffer':
-        main_config['algo_kwargs']['replay_buffer_class'] = HerReplayBuffer
+    if main_config["algo_kwargs"]["replay_buffer_class"] == "HerReplayBuffer":
+        main_config["algo_kwargs"]["replay_buffer_class"] = HerReplayBuffer
     return main_config
 
 
 def process_human_trajectories(path: Path, flatten=False, mapping: dict = None):
     trajectories = {}
     for episode in path.iterdir():
-        trajectory_path = episode / 'trajectory.npz'
+        trajectory_path = episode / "trajectory.npz"
         if not trajectory_path.exists():
             continue
-        episode_data = np.load(episode / 'trajectory.npz', allow_pickle=True)
+        episode_data = np.load(episode / "trajectory.npz", allow_pickle=True)
         episode_data = dict(episode_data)
         if flatten:
             for key, value in episode_data.items():
                 if mapping is not None:
                     if key in mapping:
                         key = mapping[key]
-                if key == 'time':
+                if key == "time":
                     continue
                 trajectories.setdefault(key, []).extend(value)
         else:
@@ -429,8 +444,7 @@ def process_human_trajectories(path: Path, flatten=False, mapping: dict = None):
     return trajectories
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     exit()
 
     # env = make_env(
