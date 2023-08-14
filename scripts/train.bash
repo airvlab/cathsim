c8512ccf3e06aaa3cea56098c41323eb5d690f06
Tudor Jianu <tudorjnu@gmail.com>
2023-08-14 14:28:44 +0100

    change train script

--- scripts/train.bash
+++ scripts/train.bash
@@ -5,11 +5,11 @@ echo "-----Training-----"
 configs=(full internal_pixels internal pixels pixels_mask)
 targets=(lcca)
 phantom=phantom3
-n_runs=3
+n_runs=6
 n_timesteps=600000
 for config in ${configs[@]}; do
 	for target in ${targets[@]}; do
 		echo "Training $config on $phantom - $target - run $run"
-		python rl/train.py --config $config --target $target --phantom $phantom --n-runs $n_runs --n-timesteps $n_timesteps
+		train --config $config --target $target --phantom $phantom --n-runs $n_runs --n-timesteps $n_timesteps
 	done
 done
