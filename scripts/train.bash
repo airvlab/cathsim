7d720295c57e26ffe8392b267ff05df6413e599d
Tudor Jianu <tudorjnu@gmail.com>
2023-08-21 14:22:53 +0100

    fix the script

--- scripts/train.bash
+++ scripts/train.bash
@@ -4,13 +4,15 @@
 echo "-----Training-----"
 configs=(full internal_pixels internal pixels pixels_mask)
 targets=(bca lcca)
-phantom=phantom3
+phantoms=(phantom3)
 trial=1
 n_runs=6
 n_timesteps=600000
 for config in ${configs[@]}; do
 	for target in ${targets[@]}; do
-		echo "Training $config on $phantom - $target"
-		train --config $config --target $target --phantom $phantom --n-runs $n_runs --n-timesteps $n_timesteps --trial test --trial-name trial --base-path ./my-test-results
+		for phantom in ${phantoms[@]}; do
+			echo "Training $config on $phantom - $target"
+			train --config $config --target $target --phantom $phantom --n-runs $n_runs --n-timesteps $n_timesteps --trial-name trial --base-path ./my-test-results -e
+		done
 	done
 done
