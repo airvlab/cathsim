2671eb85fda5baca17e1af79ad3fd87193b026a8
Tudor Jianu <tudorjnu@gmail.com>
2023-09-21 14:04:06 +0100

    add scratch

---
+++ scratch/scratch.py
@@ -0,0 +1,224 @@
+import numpy as np
+from pathlib import Path
+from scipy.interpolate import CubicSpline
+import matplotlib.pyplot as plt
+from shape_reconstruction import get_points, set_limits, P_TOP, P_SIDE
+from cathsim.dm.visualization import point2pixel
+from scipy.optimize import minimize_scalar
+from scipy import optimize
+import cv2
+
+# Load data
+DATA_PATH = Path("data/guidewire-reconstruction-2/")
+image_num = 23
+points1, points2, points_3d, geom_pos = get_points(image_num)
+top = plt.imread(DATA_PATH / f"{image_num}_top.png")
+side = plt.imread(DATA_PATH / f"{image_num}_side.png")
+
+iteration = 0
+
+
+def arc_length(x, y, z):
+    """Calculate the arc length of the curve defined by vectors x, y, z."""
+    dx = np.diff(x)
+    dy = np.diff(y)
+    dz = np.diff(z)
+    return np.sum(np.sqrt(dx**2 + dy**2 + dz**2))
+
+
+def get_curve(points_2D):
+    """Get cubic spline interpolation of 2D points."""
+    t = np.linspace(0, 1, len(points_2D))
+    cs_x = CubicSpline(t, points_2D[:, 0], bc_type='natural')
+    cs_y = CubicSpline(t, points_2D[:, 1], bc_type='natural')
+    return cs_x, cs_y
+
+
+def resample_curve_equidistant(points, distance=2.0):
+
+    total_length = arc_length(points[:, 0], points[:, 1], points[:, 2])
+
+    num_points = int(np.floor(total_length / distance))
+
+    # Create parameter vector
+    t = np.linspace(0, 1, len(points))
+
+    # Create cubic splines for x, y, z
+    cs_x = CubicSpline(t, points[:, 0], bc_type='natural')
+    cs_y = CubicSpline(t, points[:, 1], bc_type='natural')
+    cs_z = CubicSpline(t, points[:, 2], bc_type='natural')
+
+    # Initialize variables
+    new_points = []
+    last_point = points[0]
+    t_current = 0
+    dt = 0.001  # Step for parameter t
+
+    # Resample curve
+    for _ in range(num_points):
+        found = False
+        while not found:
+            t_current += dt
+            if t_current > 1.0:
+                break  # End of the curve
+
+            candidate_point = np.array([cs_x(t_current), cs_y(t_current), cs_z(t_current)])
+            distance_to_last = np.linalg.norm(candidate_point - last_point)
+
+            if distance_to_last >= distance:
+                found = True
+                new_points.append(candidate_point)
+                last_point = candidate_point
+
+    return np.array(new_points)
+
+
+def get_distance_to_curve(points_2D, curve):
+    """Get the distance from each 2D point to the curve defined by cubic splines."""
+    cs_x, cs_y = curve
+    distances = []
+
+    for point in points_2D:
+        # Objective function to minimize
+        def objective(t):
+            return np.sqrt((cs_x(t) - point[0])**2 + (cs_y(t) - point[1])**2)
+
+        # Minimize the objective function to find closest point on the curve
+        res = minimize_scalar(objective, bounds=(0, 1), method='bounded')
+
+        # The minimum distance from the point to the curve
+        min_distance = res.fun
+        distances.append(min_distance)
+
+    distances = np.array(distances)
+    return distances
+
+
+def fn_loss(points, line):
+    return get_distance_to_curve(points, line)
+
+
+def fn_aggregate(error1, error2):
+    return np.sum(error1) + np.sum(error2)
+
+
+def fn_constraint(points):
+    points = points.reshape(-1, 3)
+    return np.sum(np.linalg.norm(np.diff(points, axis=0), axis=1) - 0.002)
+
+
+def objective_function(
+    params: np.ndarray,
+    line1: np.ndarray,
+    line2: np.ndarray,
+    fn_loss: callable = fn_loss,
+    fn_aggregate: callable = fn_aggregate,
+) -> float:
+    global iteration
+
+    points_3d = params.reshape(-1, 3)
+
+    projected_points1 = point2pixel(points_3d, P_TOP)
+    projected_points2 = point2pixel(points_3d, P_SIDE)
+
+    # overlap points in top and side view
+    top_copy = top.copy()
+    side_copy = side.copy()
+    for point in projected_points1:
+        cv2.circle(top_copy, tuple(point.astype(int)), 1, (0, 255, 0), -1)
+    for point in projected_points2:
+        cv2.circle(side_copy, tuple(point.astype(int)), 1, (0, 255, 0), -1)
+
+    combined = np.concatenate((top_copy, side_copy), axis=1)
+
+    cv2.imshow('combined', combined)
+    cv2.waitKey(1)
+
+    error1 = 2 * fn_loss(projected_points1, line1)
+    error2 = 2 * fn_loss(projected_points2, line2)
+
+    if fn_aggregate is None:
+        total_error = error1 + error2
+    else:
+        total_error = fn_aggregate(error1, error2)
+
+    # constraint = fn_constraint(points_3d)
+    # total_error += 0.2 * constraint
+
+    print(f'Iteration {iteration}: {total_error}', end='\r')
+    iteration += 1
+
+    return total_error
+
+
+def optimize_projection(
+    points_3d: np.ndarray,
+    points1: np.ndarray,
+    points2: np.ndarray,
+) -> np.ndarray:
+
+    curve1 = get_curve(points1)
+    curve2 = get_curve(points2)
+    resampled_points = resample_curve_equidistant(points_3d, distance=0.002)
+
+    params_init = resampled_points.flatten()
+
+    params_opt = optimize.minimize(
+        objective_function,
+        params_init,
+        args=(curve1, curve2),
+        # method='CG',
+        options={
+            # 'maxiter': 10,
+            'disp': True,
+            # 'eps': 1e-6,
+            # 'gtol': 1e-6,
+        },
+    )
+
+    print(f'Optimization result: {params_opt}')
+    points_3d_opt = params_opt.x.reshape(-1, 3)
+
+    return points_3d_opt
+
+
+if __name__ == "__main__":
+    original_top = point2pixel(geom_pos, P_TOP)
+    # bound the points to the image
+    original_top = original_top[original_top[:, 0] < top.shape[1]]
+    original_top = original_top[original_top[:, 1] < top.shape[0]]
+    original_top = original_top[original_top[:, 0] > 0]
+    original_top = original_top[original_top[:, 1] > 0]
+
+    generated = resample_curve_equidistant(points_3d, distance=0.002)
+    reprojected = point2pixel(generated, P_TOP)
+    curve = get_curve(points1)
+    print(curve)
+    # generate the curve
+
+    # distances_original = get_distance_to_curve(points1, curve)
+    # distances_reproj = get_distance_to_curve(reprojected, curve)
+    reproj_opt = optimize_projection(points_3d, points1, points2)
+    reproj_opt_top = point2pixel(reproj_opt, P_TOP)
+    reproj_opt_side = point2pixel(reproj_opt, P_SIDE)
+    print(reproj_opt.shape)
+
+    fig, ax = plt.subplots(1, 2)
+    ax[0].imshow(top)
+    ax[0].plot(original_top[:, 0], original_top[:, 1], 'r.', label='Original')
+    ax[0].plot(reproj_opt_top[:, 0], reproj_opt_top[:, 1], 'b.', label='Reprojected')
+    ax[0].set_title('Top view')
+    ax[0].axis('off')
+    ax[1].imshow(side)
+    ax[1].plot(points2[:, 0], points2[:, 1], 'r.', label='Original')
+    ax[1].plot(reproj_opt_side[:, 0], reproj_opt_side[:, 1], 'b.', label='Reprojected')
+    ax[1].set_title('Side view')
+    ax[1].axis('off')
+
+    fig.legend(ncol=3, loc='upper center', bbox_to_anchor=(0.1, 1.1))
+    plt.show()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot(geom_pos[:, 0], geom_pos[:, 1], geom_pos[:, 2], 'r.', label='Original')
+    ax.plot(reproj_opt[:, 0], reproj_opt[:, 1], reproj_opt[:, 2], 'b.', label='Reprojected')
--- scratch/shape_reconstruction.py
+++ scratch/shape_reconstruction.py
@@ -1,43 +1,48 @@
-import cv2
-import numpy as np
 from pathlib import Path
+
+import cv2
+import matplotlib as mpl
 import matplotlib.pyplot as plt
-from cathsim.dm.visualization import point2pixel
+import numpy as np
 import trimesh
-from skimage.morphology import skeletonize, thin
+from cathsim.dm.visualization import point2pixel
+from scipy import interpolate, optimize
+from skimage.morphology import thin
+
+# set scatter marker size
+mpl.rcParams["lines.markersize"] = 1
 
 DATA_DIR = Path.cwd() / "data" / "guidewire-reconstruction-2"
 
 if not DATA_DIR.exists():
     raise FileNotFoundError(f"{DATA_DIR} does not exist.")
 
-P_top = np.array([
-    [-5.79411255e+02, 0.00000000e+00, 2.39500000e+02, -7.72573376e+01],
-    [0.00000000e+00, 5.79411255e+02, 2.39500000e+02, -1.32301407e+02],
-    [0.00000000e+00, 0.00000000e+00, 1.00000000e+00, -2.50000000e-01],
-])
+P_TOP = np.array(
+    [
+        [-5.79411255e02, 0.00000000e00, 2.39500000e02, -7.72573376e01],
+        [0.00000000e00, 5.79411255e02, 2.39500000e02, -1.32301407e02],
+        [0.00000000e00, 0.00000000e00, 1.00000000e00, -2.50000000e-01],
+    ]
+)
 
-P_side = np.array([
-    [-2.39500000e+02, 5.79411255e+02, 0.00000000e+00, -1.13528182e+02],
-    [-2.39500000e+02, 0.00000000e+00, 5.79411255e+02, -7.00723376e+01],
-    [-1.00000000e+00, 0.00000000e+00, 0.00000000e+00, -2.20000000e-01],
-])
+P_SIDE = np.array(
+    [
+        [-2.39500000e02, 5.79411255e02, 0.00000000e00, -1.13528182e02],
+        [-2.39500000e02, 0.00000000e00, 5.79411255e02, -7.00723376e01],
+        [-1.00000000e00, 0.00000000e00, 0.00000000e00, -2.20000000e-01],
+    ]
+)
 
 
-def plot3d(rec_points=None, head_pos=None):
-    mesh_path = Path.cwd() / "src/cathsim/dm/components/phantom_assets/meshes/phantom3/visual.stl"
+def set_limits(ax):
+    mesh_path = (
+        Path.cwd()
+        / "src/cathsim/dm/components/phantom_assets/meshes/phantom3/visual.stl"
+    )
     mesh = trimesh.load_mesh(mesh_path)
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection="3d")
     ax.set_xlim(mesh.bounds[0][0], mesh.bounds[1][0])
     ax.set_ylim(mesh.bounds[0][1], mesh.bounds[1][1])
     ax.set_zlim(mesh.bounds[0][2], mesh.bounds[1][2])
-    if rec_points is not None:
-        ax.scatter(rec_points[:, 0], rec_points[:, 1], rec_points[:, 2], label="Reconstructed shape", s=1)
-    if head_pos is not None:
-        ax.scatter(head_pos[:, 0], head_pos[:, 1], head_pos[:, 2], label="Actual shape", s=1)
-    fig.legend(loc="outside upper center", ncol=2)
-    plt.show()
 
 
 def read_segmented_image(file_path):
@@ -49,9 +54,9 @@ def find_endpoints(skeleton):
     for i in range(1, skeleton.shape[0] - 1):
         for j in range(1, skeleton.shape[1] - 1):
             if skeleton[i, j]:
-                sum_neighbors = np.sum(skeleton[i - 1:i + 2, j - 1:j + 2])
+                sum_neighbors = np.sum(skeleton[i - 1 : i + 2, j - 1 : j + 2])
                 if sum_neighbors == 2:  # The pixel itself + one neighbor = 2
-                    print(f"Found endpoint at ({j}, {i})")
+                    # print(f"Found endpoint at ({j}, {i})")
                     endpoints.append((j, i))
     return endpoints
 
@@ -87,7 +92,9 @@ def get_backbone_points(image, spacing=1):
 
 
 def compute_gradient(P_3d, P_2d_observed, P_matrix, alpha=1.0):
-    homogeneous_P_3d = np.hstack([P_3d, np.ones((P_3d.shape[0], 1))])  # Convert to homogeneous coordinates
+    homogeneous_P_3d = np.hstack(
+        [P_3d, np.ones((P_3d.shape[0], 1))]
+    )  # Convert to homogeneous coordinates
     P_2d_reprojected = np.dot(P_matrix, homogeneous_P_3d.T).T
     P_2d_reprojected /= P_2d_reprojected[:, 2][:, np.newaxis]
 
@@ -102,7 +109,9 @@ def compute_gradient(P_3d, P_2d_observed, P_matrix, alpha=1.0):
     return P_3d_new
 
 
-def reprojection_error_and_gradient(points_3d, points2d_1, points2d_2, P1, P2, alpha=1.0):
+def reprojection_error_and_gradient(
+    points_3d, points2d_1, points2d_2, P1, P2, alpha=1.0
+):
     error_1 = np.sum((points2d_1 - point2pixel(points_3d, P1)) ** 2)
     error_2 = np.sum((points2d_2 - point2pixel(points_3d, P2)) ** 2)
     total_error = error_1 + error_2
@@ -167,64 +176,294 @@ def triangulate_points(P1, P2, x1s, x2s):
     return Xs
 
 
-if __name__ == "__main__":
+def reprojection_error(
+    params: np.ndarray,
+    points1: np.ndarray,
+    points2: np.ndarray,
+    P_top: np.ndarray,
+    P_side: np.ndarray,
+    fn_loss: callable,
+    fn_aggregate: callable = None,
+) -> np.ndarray:
+    # Reshape params into 3D points
+    points_3d = params.reshape(-1, 3)
 
-    image_num = 23
-    head_pos = DATA_DIR / f"{image_num}_geom_pos.npy"
-    head_pos = np.load(head_pos)
-    spacing = 15
-    size = 10
+    # Project the 3D points back to 2D points using projection matrices
+    projected_points1 = point2pixel(points_3d, P_top)
+    projected_points2 = point2pixel(points_3d, P_side)
 
+    assert (
+        projected_points1.shape == points1.shape
+        and projected_points2.shape == points2.shape
+    ), (
+        f"projected_points1.shape = {projected_points1.shape}, points1.shape = {points1.shape}, "
+        f"projected_points2.shape = {projected_points2.shape}, points2.shape = {points2.shape}"
+    )
+
+    # Compute the reprojection error
+    error1 = fn_loss(points1, projected_points1)
+    error2 = fn_loss(points2, projected_points2)
+
+    if fn_aggregate is None:
+        return error1 + error2
+    total_error = fn_aggregate(error1, error2)
+    print(f"Total error: {total_error}")
+
+    return total_error
+
+
+def manhattan_distance(x1, x2):
+    return np.sum(np.abs(x1 - x2), axis=-1)
+
+
+def huber_loss(y_true, y_pred, delta=1.0):
+    error = y_true - y_pred
+    abs_error = np.abs(error)
+    quadratic = np.minimum(abs_error, delta)
+    linear = abs_error - quadratic
+    return np.sum(0.5 * quadratic**2 + delta * linear, axis=-1)
+
+
+def aggregate_error(error1, error2):
+    return np.sum(error1) + np.sum(error2)
+
+
+def get_points(
+    image_num: int = 23, spacing: int = 15, size: int = 10, debug: bool = False
+):
     img1 = read_segmented_image(DATA_DIR / f"{image_num}_top.png")
     img2 = read_segmented_image(DATA_DIR / f"{image_num}_side.png")
+
     points1 = get_backbone_points(img1, spacing=spacing)
     points2 = get_backbone_points(img2, spacing=spacing)
     min_length = min(len(points1), len(points2))
     points1 = points1[:min_length]
     points2 = points2[:min_length]
-    print(f"Number of points: {len(points1)}")
 
-    img1_w_points = plot_over_image(img1, points1, alternate_color=True, size=size)
-    img2_w_points = plot_over_image(img2, points2, alternate_color=True, size=size)
-
-    # fig, ax = plt.subplots(1, 2)
-    # ax[0].imshow(img1_w_points)
-    # ax[1].imshow(img2_w_points)
-    # ax[0].title.set_text("Top")
-    # ax[1].title.set_text("Side")
-    # ax[0].axis("off")
-    # ax[1].axis("off")
-    # plt.show()
-
-    # points_3d_h = cv2.triangulatePoints(P_top, P_side, points1.T, points2.T)
-    points_3d_h = triangulate_points(P_top, P_side, points1, points2)
-    print(points_3d_h.shape)
+    points_3d_h = triangulate_points(P_TOP, P_SIDE, points1, points2)
     mask = valid_points(points_3d_h)
     if mask is not None:
         points1 = points1[mask]
         points2 = points2[mask]
         points_3d_h = points_3d_h[mask]
 
-    print(points_3d_h.shape)
-
     points_3d = points_3d_h[:, :3] / points_3d_h[:, 3, np.newaxis]
 
-    reprojected_top = point2pixel(points_3d, P_top)
-    reprojected_side = point2pixel(points_3d, P_side)
+    if debug:
+        print(points_3d_h.shape)
+        img1_w_points = plot_over_image(img1, points1, alternate_color=True, size=size)
+        img2_w_points = plot_over_image(img2, points2, alternate_color=True, size=size)
+        combined = np.hstack([img1_w_points, img2_w_points])
+        plt.imshow(combined)
+        plt.axis("off")
+    assert (
+        points1.shape == points2.shape and points2.shape[0] == points_3d.shape[0]
+    ), f"points1.shape = {points1.shape}, points2.shape = {points2.shape}, points_3d.shape = {points_3d.shape}"
+    return points1, points2, points_3d, np.load(DATA_DIR / f"{image_num}_geom_pos.npy")
 
-    img1_w_points = plot_over_image(img1, reprojected_top, alternate_color=True, size=size)
-    img2_w_points = plot_over_image(img2, reprojected_side, alternate_color=True, size=size)
-    combined = np.hstack([img1_w_points, img2_w_points])
-    plt.imshow(combined)
-    plt.axis("off")
 
-    __import__('pprint').pprint(points_3d)
-    plot3d(points_3d, head_pos)
+def optimize_projection(
+    points_3d: np.ndarray,
+    points1: np.ndarray,
+    points2: np.ndarray,
+    P1: np.ndarray = P_TOP,
+    P2: np.ndarray = P_SIDE,
+) -> np.ndarray:
+    # Flatten the 3D points as initial parameters
+    params_init = points_3d.flatten()
+    # optimize the 3D points
+    params_opt = optimize.minimize(
+        reprojection_error,
+        params_init,
+        args=(points1, points2, P_TOP, P_SIDE, manhattan_distance, aggregate_error),
+        method="CG",
+        options={
+            "maxiter": 1e7,
+            "disp": True,
+            "eps": 1e-6,  # Step size used for numerical approximation of the Jacobian
+            "gtol": 1e-6,  # Tolerance for termination by the change of the objective function value
+        },
+    )
 
-    alpha = 0.003  # Learning rate, you may need to adjust this
-    num_iterations = 10  # Number of iterations
-    for i in range(num_iterations):
-        total_error, gradient = reprojection_error_and_gradient(points_3d, points1, points2, P_top, P_side, alpha=alpha)
-        print(f"Iteration {i+1}: Total Error = {total_error}",)
-        points_3d -= alpha * gradient
-    # plot3d(points_3d, head_pos)
+    print(f"Optimization result: {params_opt}")
+    points_3d_opt = params_opt.x.reshape(-1, 3)
+
+    return points_3d_opt
+
+
+def fit_3d_curve(points_3d):
+    rbfi = interpolate.Rbf(points_3d[:, 0], points_3d[:, 1], points_3d[:, 2])
+    return rbfi
+
+
+def point_on_curve(curve_function, t):
+    # Get a 3D point corresponding to parameter t on the curve
+    return curve_function(t)
+
+
+def reconstruct_3d_points(params):
+    pass
+
+
+def measure_distance_to_backbone(projected_points, backbone):
+    pass
+
+
+def spacing_constraint(points_3d, target_spacing=0.002):
+    distances = np.linalg.norm(np.diff(points_3d, axis=0), axis=1)
+    return np.sum(np.abs(distances - target_spacing))
+
+
+def objective_function(
+    params: np.ndarray,
+    points1: np.ndarray,
+    points2: np.ndarray,
+    P1: np.ndarray,
+    P2: np.ndarray,
+    fn_loss: callable,
+    fn_aggregate: callable = None,
+    fn_constraint: callable = None,
+) -> float:
+    points_3d = params.reshape(-1, 3)
+    projected_points1 = point2pixel(points_3d, P1)
+    projected_points2 = point2pixel(points_3d, P2)
+
+    error1 = fn_loss(points1, projected_points1)
+    error2 = fn_loss(points2, projected_points2)
+
+    if fn_aggregate is None:
+        total_error = error1 + error2
+    else:
+        total_error = fn_aggregate(error1, error2)
+
+    if fn_constraint is not None:
+        constraint_error = fn_constraint(points_3d)
+        total_error += constraint_error
+
+    return total_error
+
+
+def optimize_projection_2(
+    points_3d: np.ndarray,
+    points1: np.ndarray,
+    points2: np.ndarray,
+    P1: np.ndarray = P_TOP,
+    P2: np.ndarray = P_SIDE,
+    fn_loss: callable = manhattan_distance,
+    fn_aggregate: callable = aggregate_error,
+    fn_constraint: callable = spacing_constraint,
+) -> np.ndarray:
+    params_init = points_3d.flatten()
+
+    params_opt = optimize.minimize(
+        objective_function,
+        params_init,
+        args=(points1, points2, P1, P2, fn_loss, fn_aggregate, fn_constraint),
+        method="CG",
+        options={
+            "maxiter": 1e7,
+            "disp": True,
+            "eps": 1e-6,
+            "gtol": 1e-6,
+        },
+    )
+
+    print(f"Optimization result: {params_opt}")
+    points_3d_opt = params_opt.x.reshape(-1, 3)
+
+    return points_3d_opt
+
+
+def make_3d_curve(points_3d):
+    # Compute pairwise distances and total length of the piecewise linear path
+    dists = np.sqrt(np.sum(np.diff(points_3d, axis=0) ** 2, axis=1))
+    total_length = np.sum(dists)
+
+    # Compute the arc-length parameterization
+    u = np.zeros_like(dists)
+    u[1:] = np.cumsum(dists[:-1]) / total_length
+
+    # Fit polynomial curves in x, y, z separately
+    degree = 5  # for example
+    p_x = np.polyfit(u, points_3d[:-1, 0], degree)
+    p_y = np.polyfit(u, points_3d[:-1, 1], degree)
+    p_z = np.polyfit(u, points_3d[:-1, 2], degree)
+
+    # Resample at uniform intervals along the curve
+    num_points = int(total_length / 0.002)  # 2mm spacing
+    u_new = np.linspace(0, 1, num_points)
+    x_new = np.polyval(p_x, u_new)
+    y_new = np.polyval(p_y, u_new)
+    z_new = np.polyval(p_z, u_new)
+    points_3d_new = np.column_stack((x_new, y_new, z_new))
+
+    # Plotting
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    ax.scatter(
+        points_3d[:, 0],
+        points_3d[:, 1],
+        points_3d[:, 2],
+        c="b",
+        label="Original points",
+        s=1,
+    )
+    ax.plot(
+        points_3d_new[:, 0],
+        points_3d_new[:, 1],
+        points_3d_new[:, 2],
+        c="r",
+        label="Fitted curve",
+    )
+    set_limits(ax)
+
+    return fig, ax
+
+
+def interpolate_even_spacing(chain, spacing=0.002):
+    new_chain = [chain[0]]
+    leftover_distance = 0
+
+    for i in range(len(chain) - 1):
+        start = chain[i]
+        end = chain[i + 1]
+
+        segment = end - start
+        segment_length = np.linalg.norm(segment)
+
+        num_points = int((segment_length + leftover_distance) // spacing)
+        leftover_distance += segment_length - num_points * spacing
+
+        for j in range(1, num_points + 1):
+            fraction = (j * spacing - leftover_distance) / segment_length
+            new_point = start + fraction * segment
+            new_chain.append(new_point)
+
+    return np.array(new_chain)
+
+
+if __name__ == "__main__":
+    points1, points2, points_3d, geom_pos = get_points()
+    image = read_segmented_image(DATA_DIR / "23_side.png")
+    # points_3d_opt = optimize_projection_2(points_3d, points1, points2)
+    fig, ax = make_3d_curve(points_3d)
+    # equidistant_points = interpolate_even_spacing(points_3d)
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    set_limits(ax)
+    # ax.scatter(points_3d[:, 0], points_3d[:, 1], points_3d[:, 2], c='r', label='Original points', s=1)
+    # ax.scatter(equidistant_points[:, 0], equidistant_points[:, 1], equidistant_points[:, 2], c='b', label='Equidistant points', s=1)
+    ax.scatter(
+        geom_pos[:, 0], geom_pos[:, 1], geom_pos[:, 2], c="g", label="Geom points", s=1
+    )
+    ax.legend(ncol=3)
+    plt.show()
+
+    # points_3d_opt = optimize_projection(points_3d, points1, points2)
+    # side_actual = plot_over_image(image, point2pixel(geom_pos, P_SIDE), alternate_color=True, size=3)
+    # side_points = plot_over_image(image, points2, alternate_color=True, size=3)
+    # side_reprojected_opt = plot_over_image(image, point2pixel(points_3d_opt, P_SIDE), alternate_color=True, size=3)
+    # combined = np.hstack([side_actual, side_points, side_reprojected_opt])
+    # plt.imshow(combined)
+    # plt.axis("off")
+    # plt.show()
