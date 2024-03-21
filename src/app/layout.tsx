ef31bb2ad70bc45b940a2a40987ddee8b5ece54c
Tudor Jianu <tudorjnu@gmail.com>
2024-03-21 10:19:33 +0000

    add initial template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from "@/app/providers";
+import MyNavBar from "./navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="light">
+      <body className={inter.className}>
+        <MyNavBar />
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
