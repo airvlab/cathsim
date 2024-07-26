b19b4a983366ac3c95038007e16e706f01d84efc
Tudor Jianu <tudorjnu@gmail.com>
2024-07-26 12:54:56 +0100

    fix env

--- src/cathsim/envs/__init__.py
+++ src/cathsim/envs/__init__.py
@@ -1,9 +1,1 @@
-from cathsim.gym.envs.cathsim import CathSim
-from gymnasium.envs.registration import register
-
-register(
-    id="cathsim/CathSim-v0",
-    entry_point="cathsim.gym.envs:CathSim",
-    max_episode_steps=300,
-    nondeterministic=True,
-)
+from cathsim.envs.cathsim_v1 import CathSim
